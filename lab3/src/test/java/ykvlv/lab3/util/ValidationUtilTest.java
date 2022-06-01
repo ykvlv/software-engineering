98c4846e620067df835e737eb90bcbc655dc9fbe
Arina <arinalun14@gmail.com>
2022-06-01 09:26:36 +0300

    ValidationUtilTest updated

--- lab3/src/test/java/ykvlv/lab3/util/ValidationUtilTest.java
+++ lab3/src/test/java/ykvlv/lab3/util/ValidationUtilTest.java
@@ -1,24 +1,23 @@
 package ykvlv.lab3.util;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidationUtilTest {
 
-    @Test
-    void checkY() {
-        assertFalse(ValidationUtil.checkY("a"));
-        assertFalse(ValidationUtil.checkY("-0"));
-        assertFalse(ValidationUtil.checkY("13"));
-        assertFalse(ValidationUtil.checkY("-13"));
-        assertFalse(ValidationUtil.checkY("-"));
-        assertFalse(ValidationUtil.checkY("5.1"));
-
-        assertTrue(ValidationUtil.checkY("-0.231"));
-        assertTrue(ValidationUtil.checkY("-0.231748327492384723987432987427374293749"));
-        assertTrue(ValidationUtil.checkY("-5"));
-        assertTrue(ValidationUtil.checkY("3"));
-        assertTrue(ValidationUtil.checkY("0"));
+    @ParameterizedTest
+    @ValueSource(strings = {"-0.231", "-0.231748327492384723987432987427374293749", "-5", "3", "0"})
+    void checkYTrue(String input) {
+        assertTrue(ValidationUtil.checkY(input));
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "-0", "13", "-13", "-", "5.1"})
+    void checkYFalse(String input) {
+        assertFalse(ValidationUtil.checkY(input));
+    }
+
+
 }
